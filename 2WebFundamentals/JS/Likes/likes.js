52787f1c225e0134901a79214007d709e6eb4ae3
Allen Nowak <2123918+AllenNowak@users.noreply.github.com>
2023-04-18 00:03:59 -0700

    javascript functioning for both parts 1 & 2

--- 2WebFundamentals/JS/Likes/likes.js
+++ 2WebFundamentals/JS/Likes/likes.js
@@ -1,7 +1,13 @@
-function likeUser(userId) {
-    var currentCountElement = document.querySelector("#" + userId);
+function likeUniqueUsers(userId) {
+    // This is a stand-in function
+    // I expect that we'd never just take the argument without scrubbing potentially hazardous inputs
+    likeSomething('#' + userId)
+}
+
+function likeSomething(idString) {
+    var currentCountElement = document.querySelector(idString);
     let count = parseInt(currentCountElement.innerText);
     count++;
-    console.log(`Count now ${count}`)
+
     currentCountElement.innerText = `${count}`;
 }
