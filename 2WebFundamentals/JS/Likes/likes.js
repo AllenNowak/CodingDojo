3f67cb1714d2f433b291e05738e055e11afc5417
Allen Nowak <2123918+AllenNowak@users.noreply.github.com>
2023-04-17 19:13:33 -0700

    Likes pt 1 complete

--- 2WebFundamentals/JS/Likes/likes.js
+++ 2WebFundamentals/JS/Likes/likes.js
@@ -1,1 +1,7 @@
-/* here */
+function likeUser(userId) {
+    var currentCountElement = document.querySelector("#" + userId);
+    let count = parseInt(currentCountElement.innerText);
+    count++;
+    console.log(`Count now ${count}`)
+    currentCountElement.innerText = `${count}`;
+}
