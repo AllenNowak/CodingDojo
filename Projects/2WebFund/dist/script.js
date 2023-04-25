c6d8190b1c1c04d802e7e81456c0890d3b6d0630
Allen Nowak <2123918+AllenNowak@users.noreply.github.com>
2023-04-24 21:44:25 -0700

    Initial Commit

---
+++ Projects/2WebFund/dist/script.js
@@ -0,0 +1,9 @@
+/*
+ TODO:
+    Set up the Mock Data pipeline
+    Register for an API key
+    Determine what functions I need to implement to make the pipeline work
+    Periodic Calls via Timeout() ? Or install Node.js & WebSockets?
+
+    Add in Chartjs once mock data is present
+ */
